2156e316f914a59016db306315ae487bdd9776d9
pete consuegra <peteconsuegra@petes-MacBook-Pro.local>
2019-04-09 06:34:47 -0500

    first commit to packagist

---
+++ testpackage.php
@@ -0,0 +1,16 @@
+<?php
+
+/* 
+ * Created by : Digvijay
+ * ProjectName : TestPackage
+ * Purpose : Submit to packagist
+ */
+
+namespace testPackage;
+
+class testPackage {
+    public function pack() {
+        return "Hello Packagist! I am coming";
+    }
+}
+
